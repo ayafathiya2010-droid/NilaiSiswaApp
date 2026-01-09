8dd57f1cdbaca6ab5330e14878e7e7e6faec87da
ayafathiya2010-droid <ayafathiya2010@gmail.com>
2026-01-09 14:29:03 +0700

    menambahkan input nilai dan nama

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 string nama;
 
 Console.WriteLine("Aplikasi nilai siswa\n");
+
+Console.WriteLine("Nama siswa: ");
+nama = Console.ReadLine();
+
+Console.WriteLine("Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
